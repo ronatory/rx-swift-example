881e18c920c26cbd10e039a8ab1d206ab0e0c55b
ronatory <ronatory@gmail.com>
2016-09-26 23:31:58 +0200

    Add dynamic search via rxswift and rxcocoa

--- RxSwiftExample/TableViewController.swift
+++ RxSwiftExample/TableViewController.swift
@@ -7,23 +7,35 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class TableViewController: UITableViewController {
 	
 	@IBOutlet weak var searchBar: UISearchBar!
 	
 	var shownCities = [String]() // Data source for UITableView
-	let allCities = ["New York", "London", "Oslo", "Warsaw", "Berlin", "Praga"] // Mocked API data
+	let allCities = ["New York", "London", "Oslo", "Warsaw", "Frankfurt", "Prag", "Berlin", "Philadelphia", "Sao Paulo", "Milan", "Manila", "Tokio", "Los Angeles", "Paris", "Portland"] // Mocked API data
+	let disposeBag = DisposeBag() // Bag of disposables to release them when view is being deallocated (protect agains retain cycle)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+		
+		self.title = "Search Cities"
+		
 		tableView.dataSource = self
+		
+		searchBar
+			.rx_text // observable property
+			.throttle(0.5, scheduler: MainScheduler.instance) // wait 0.5 seconds for changes
+			.distinctUntilChanged() // check if the new value is the same as the old one
+			.filter { $0.characters.count > 0 } // filter for a non-empty query
+			.subscribeNext { [unowned self] (query) in // get notified of every new value
+				self.shownCities = self.allCities.filter { $0.hasPrefix(query) } // do the "API Request" to find cities
+				self.tableView.reloadData() // reload data in table view
+			}
+			.addDisposableTo(disposeBag)
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,51 +63,5 @@ class TableViewController: UITableViewController {
 
         return cell
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
